8512045d5e1978045cd8b52c4315df95c2e21684
Балахонцева Анастасия <ayamileto@gmail.com>
2023-06-27 04:08:34 +0300

    add channel data, updated print info and init

--- src/channel.py
+++ src/channel.py
@@ -14,9 +14,23 @@ class Channel:
         if not isinstance(channel_id, str):
             raise ValueError('channel_id должно быть строкой')
         self.channel_id = channel_id
+        self.title = self.channel_data['items'][0]['snippet']['title']
+        self.description = self.channel_data['items'][0]['snippet']['description']
+        self.custom_url = self.channel_data['items'][0]['snippet']['customUrl']
+        self.url = f"https://www.youtube.com/{self.custom_url}"
+        print(self.url)
+        self.subscriber_count = int(self.channel_data['items'][0]['statistics']['subscriberCount'])
+        self.video_count = int(self.channel_data['items'][0]['statistics']['videoCount'])
+        self.view_count = int(self.channel_data['items'][0]['statistics']['viewCount'])
+
+    @property
+    def channel_data(self):
+        """ Получаем список из данных по API-запросу """
+        channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        return channel
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        channel = self.channel_data
         print(json.dumps(channel, indent=2, ensure_ascii=False))
 
