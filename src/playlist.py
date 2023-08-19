852dc8a4fd1cdae6fab70893947d01324dc9a6d1
Балахонцева Анастасия <ayamileto@gmail.com>
2023-08-20 01:45:49 +0300

    add describe str methods, change total_duration and add video_response and show_best_video

--- src/playlist.py
+++ src/playlist.py
@@ -7,6 +7,7 @@ from googleapiclient.discovery import build
 api_key: str = os.getenv('YT_API_KEY')
 youtube = build('youtube', 'v3', developerKey=api_key)
 
+
 class PlayList:
     """
     Класс для работы с плейлистами, инициализируется _id_ плейлиста
@@ -57,18 +58,22 @@ class PlayList:
         return video_ids
 
     @property
+    def video_response(self):
+        """ Возвращают данные всех видео из плейлиста по их ID """
+        video_response = youtube.videos().list(
+            part='contentDetails, statistics',
+            id=','.join(self.get_video_ids())
+        ).execute()
+        return video_response
+
+    @property
     def total_duration(self):
         """
         Возвращает объект класса `datetime.timedelta` с суммарной длительностью плейлиста
         """
-        video_response = youtube.videos().list(
-            part='contentDetails',
-            id=','.join(self.get_video_ids())
-        ).execute()
-
         total_duration = datetime.timedelta(seconds=0)
 
-        for video in video_response['items']:
+        for video in self.video_response['items']:
             iso_8601_duration = video['contentDetails']['duration']
             duration = isodate.parse_duration(iso_8601_duration)
             total_duration += duration
@@ -79,6 +84,19 @@ class PlayList:
         """
         Возвращает ссылку на самое популярное видео из плейлиста (по количеству лайков)
         """
-        pass #
+        best_video = None
+        max_likes = -1
 
+        for video in self.video_response['items']:
+            if 'likeCount' in video['statistics']:
+                likes = int(video['statistics']['likeCount'])
+                if likes > max_likes:
+                    max_likes = likes
+                    best_video = video
 
+        if best_video:
+            video_id = best_video['id']
+            video_url = f"https://youtu.be/{video_id}"
+            return video_url
+        else:
+            return None
